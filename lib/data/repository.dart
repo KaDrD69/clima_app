10ba3813f8bafc8f465710af8a267a065cee0e47
KaDrD <kadrd@localhost.localdomain>
2025-06-20 18:40:03 -0300

    usando APIs

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
-import 'dart:ui_web';
-
 import 'package:clima_app/data/mi_data.dart';
 import 'package:clima_app/data/model/clima_response.dart';
 import 'package:http/http.dart' as http;
 
 class Repository {
 
-  Future<ClimaResponse?> fetchClima(String ciudad) async {
+  Future<ClimaResponse?> fetchClima() async {
+    const String ciudad = "Punta Arenas";
     final url = "https://api.openweathermap.org/data/2.5/weather?q=$ciudad&appid=${ApiKey.apiKey}&units=metric";
     final response = await http.get(Uri.parse(url));
     
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,40 @@
 import 'dart:ui';
 
-import 'package:clima_app/data/model/clima_response.dart';
+//import 'package:clima_app/data/model/clima_response.dart';
 import 'package:clima_app/data/repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double? temp;
+
+  @override
+  void initState(){
+    super.initState();
+    cargarClima();
+  }
+
+  Future<void> cargarClima() async {
+    final repo = Repository();
+    final clima = await repo.fetchClima();
+    if (clima != null){
+      setState(() {
+        temp = clima.temp;
+      });
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    final repository = Repository();
-    ClimaResponse? clima = await repository.fetchClima("Punta Arenas");
-     
+       
     return Scaffold(
       backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
@@ -23,81 +45,86 @@ class HomeScreen extends StatelessWidget {
           statusBarBrightness: Brightness.dark
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Stack(
-            children: [
-              Align(
-                alignment: AlignmentDirectional(8, -0.1),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blueAccent
-                  ),
-                ),
-              ),
-              Align(
-                alignment: AlignmentDirectional(-8, -0.1),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blueAccent
-                  ),
-                ),
-              ),
-              Align(
-                alignment: AlignmentDirectional(0, -1.22),
-                child: Container(
-                  height: 300,
-                  width: 600,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent
-                  ),
-                ),
-              ),
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 200.0, sigmaY: 200.0),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.transparent
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Estado del clima",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
+      body: RefreshIndicator(
+        onRefresh: cargarClima,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: AlignmentDirectional(8, -0.1),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueAccent
                     ),
-                    Text(
-                      "PUNTA ARENAS",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Image.asset("assets/moderno/night_full_moon_rain.png")
-                  ],
+                  ),
                 ),
-              )
-            ],
-          ),
-        ),  
+                Align(
+                  alignment: AlignmentDirectional(-8, -0.1),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueAccent
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, -1.22),
+                  child: Container(
+                    height: 300,
+                    width: 600,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent
+                    ),
+                  ),
+                ),
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 200.0, sigmaY: 200.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.transparent
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        temp == null
+                        ? "cargando temperatura"
+                        : "Temperatura: ${temp!.toStringAsFixed(1)} Celcius",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Text(
+                        "PUNTA ARENAS",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Image.asset("assets/moderno/night_full_moon_rain.png")
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),  
+        ),
       ),
         
     );
