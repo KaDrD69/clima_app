ce5f6699db6dad32aff9f6af7a7949a881af83f2
KaDrD69 <debianf@protonmail.com>
2025-07-10 22:37:42 -0300

    cambios locales

--- lib/bloc/clima_bloc.dart
+++ lib/bloc/clima_bloc.dart
@@ -6,8 +6,8 @@ class ClimaBloc {
   final _climaController = StreamController();
   double _temp = 0;
   String _des = "";
-  int _id = 0;
-  String _icono = "";
+//  int _id = 0;
+//  String _icono = "";
   int _dt = 0;
   String _dianoche = "";
   String _ciudad = "";
@@ -21,7 +21,7 @@ class ClimaBloc {
     if (clima != null){    
         _temp = clima.temp;
         _des = clima.description;
-        _id = clima.id;
+        //_id = clima.id;
         _dt = clima.dt;
         _ciudad = clima.ciudad;
         if (_dt >= clima.amanacer && _dt < clima.atardecer){
@@ -29,11 +29,11 @@ class ClimaBloc {
         } else {
           _dianoche = "noche";
         }
-        //_icono = obtenerIcono(_dianoche, _id);
+      //  _icono = obtenerIcono(_dianoche, _id);
         _climaController.sink.add({
           "temp": _temp,
           "des": _des,
-          "icono": _icono,
+        //  "icono": _icono,
           "dianoche": _dianoche,
           "ciudad": _ciudad,
         });
