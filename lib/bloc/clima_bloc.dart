5cd2152d28546ecb177d658082b26f8936a0eeb3
KaDrD69 <debianf@protonmail.com>
2025-07-13 15:28:06 -0300

    update

--- lib/bloc/clima_bloc.dart
+++ lib/bloc/clima_bloc.dart
@@ -4,6 +4,7 @@ import 'package:clima_app/data/repository.dart';
 
 class ClimaBloc {
   final _climaController = StreamController();
+
   double _temp = 0;
   double _viento = 0;
   double _tempmin = 0;
@@ -17,46 +18,80 @@ class ClimaBloc {
   String _dianoche = "";
   String _ciudad = "";
 
+  final int _fecha = 0;
+
   Stream get stream => _climaController.stream;
 
   Future<void> cargarClima() async {
     final repo = Repository();
     final clima = await repo.fetchClima();
+    final climaForecast = await repo.fetchClimaForecast();
 
-    if (clima != null){    
-        _temp = clima.temp;
-        _humedad = clima.humedad;
-        _viento = clima.speedW;
-        _tempmin = clima.tempmin;
-        _tempmax = clima.tempmax;
-        _des = clima.description;
-        _desActual = clima.desActual;
-        //_id = clima.id;
-        _dt = clima.dt;
-        _ciudad = clima.ciudad;
-        if (_dt >= clima.amanacer && _dt < clima.atardecer){
-          _dianoche = "dia";
-        } else {
-          _dianoche = "noche";
-        }
-      //  _icono = obtenerIcono(_dianoche, _id);
-        _climaController.sink.add({
-          "temp": _temp,
-          "tempmin": _tempmin,
-          "tempmax": _tempmax,
-          "humedad": _humedad,
-          "viento" : _viento,
-          "des": _des,
-          "desActual": _desActual,
-        //  "icono": _icono,
-          "dianoche": _dianoche,
-          "ciudad": _ciudad,
-        });
-     
+
+    if (clima != null && climaForecast != null) {
+      _climaController.sink.add({
+        "fecha": climaForecast.fecha,
+        "temp": clima.temp,
+        "tempmin": clima.tempmin,
+        "tempmax": clima.tempmax,
+        "humedad": clima.humedad,
+        "viento": clima.speedW,
+        "des": clima.description,
+        "dt": clima.dt,
+        "ciudad": clima.ciudad,
+        "dianoche": clima.dt >= clima.amanacer && clima.dt < clima.atardecer
+            ? "dia"
+            : "noche",
+      });
+    } else {
+      _climaController.sink.add({
+        "fecha": climaForecast?.fecha ?? _fecha,
+        "temp": _temp,
+        "tempmin": _tempmin,
+        "tempmax": _tempmax,
+        "humedad": _humedad,
+        "viento": _viento,
+        "des": _des,
+        "desActual": _desActual,
+        "dianoche": _dianoche,
+        "ciudad": _ciudad,
+      });
     }
+
+    // if (clima != null) {
+    //   _temp = clima.temp;
+    //   _humedad = clima.humedad;
+    //   _viento = clima.speedW;
+    //   _tempmin = clima.tempmin;
+    //   _tempmax = clima.tempmax;
+    //   _des = clima.description;
+    //   _desActual = clima.desActual;
+    //   //_id = clima.id;
+    //   _dt = clima.dt;
+    //   _ciudad = clima.ciudad;
+    //   if (_dt >= clima.amanacer && _dt < clima.atardecer) {
+    //     _dianoche = "dia";
+    //   } else {
+    //     _dianoche = "noche";
+    //   }
+
+    //   //  _icono = obtenerIcono(_dianoche, _id);
+    //   _climaController.sink.add({
+    //     "temp": _temp,
+    //     "tempmin": _tempmin,
+    //     "tempmax": _tempmax,
+    //     "humedad": _humedad,
+    //     "viento": _viento,
+    //     "des": _des,
+    //     "desActual": _desActual,
+    //     //  "icono": _icono,
+    //     "dianoche": _dianoche,
+    //     "ciudad": _ciudad,
+    //   });
+    // }
   }
 
   void dispose() {
-  _climaController.close();
+    _climaController.close();
   }
 }
--- lib/data/model/clima_response_forecast.dart
+++ lib/data/model/clima_response_forecast.dart
@@ -1,13 +1,19 @@
 class ClimaResponseForecast {
-  final String fecha;
+  final int fecha;
+//  final List lista;
 
   ClimaResponseForecast({
     required this.fecha,
+//    required this.lista,
   });
+  
 
   factory ClimaResponseForecast.fromJson(Map<String, dynamic> json){
+    for (dynamic lista in json["list"]){
+      print(lista);
+    }
     return ClimaResponseForecast(
-      fecha: json["list"]["2"]["dt_txt"] as String,
+      fecha: json["list"][2]["dt"] as int,
     );
   }
 }
