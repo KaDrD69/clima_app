9180a936e8e3b1976773c04ae77cf90856a85b8d
KaDrD69 <debianf@protonmail.com>
2025-07-15 15:53:44 -0300

    update

--- lib/data/model/clima_response_forecast.dart
+++ lib/data/model/clima_response_forecast.dart
@@ -1,13 +1,35 @@
 class ClimaResponseForecast {
   final List<Map<String, dynamic>> lista;
+  // final int dia2;
+  // final int dia3;
+  // final int dia4;
+  // final int dia2Temp;
+  // final int dia3Temp;
+  // final int dia4Temp;
 
   ClimaResponseForecast({
     required this.lista,
+    // required this.dia2,
+    // required this.dia3,
+    // required this.dia4,
+    // required this.dia2Temp,
+    // required this.dia3Temp,
+    // required this.dia4Temp,
   });
-  
 
-  factory ClimaResponseForecast.fromJson(Map<String, dynamic> json){
+  // final Map<int, String> _diaSemana = {
+  //   1: "Lunes",
+  //   2: "Martes",
+  //   3: "Miercoles",
+  //   4: "Jueves",
+  //   5: "Viernes",
+  //   6: "Sábado",
+  //   7: "Domingo",
+  // };
+
+  factory ClimaResponseForecast.fromJson(Map<String, dynamic> json) {
     final listaLimpia = List<Map<String, dynamic>>.from(json["list"]);
+
     return ClimaResponseForecast(
       lista: listaLimpia,
     );
